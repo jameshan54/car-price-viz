6216a452bfda4ef728e00622a594e14a01f8ea9f
James Han <jameshan5040@gmail.com>
2025-07-11 21:47:28 -0500

    update

--- data/data preprocessing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Sampling
-df = pd.read_csv("data/car_sales_data.csv")
-
-sampled_df = df.sample(n=1000000, random_state=42)
-
-sampled_df.to_csv("data/car_sales_data_sampled.csv", index=False)
